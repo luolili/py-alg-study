c9d26ad2164ee388186847129bccc85f4ae2160f
Administrator <2084267015@qq.com>
2020-06-21 19:14:24 +0800

    alg

--- two_sum.py
+++ two_sum.py
@@ -1,1 +1,13 @@
-print('hello git')
+from typing import List
+
+
+class TwoSum:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map = {}
+        for index, num in enumerate(nums):
+            another_num = target - num
+            if another_num in map:
+                return [map[num], index]
+            else:
+                map[num] = index
+        return None
