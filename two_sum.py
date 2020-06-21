66786983e92fb1bc9f19e72f7851ee80d30fa337
Administrator <2084267015@qq.com>
2020-06-21 19:46:43 +0800

    alg

--- two_sum.py
+++ two_sum.py
@@ -5,9 +5,15 @@ class TwoSum:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         map = {}
         for index, num in enumerate(nums):
-            another_num = target - num
-            if another_num in map:
-                return [map[num], index]
-            else:
-                map[num] = index
-        return None
+            map[num] = index
+        for i, n in enumerate(nums):
+            j = map.get(target - n)
+            if j is not None and i != j:
+                return [i, j]
+
+
+if __name__ == '__main__':
+    t = TwoSum()
+    nums = [1, 2, 3, 4, 5, 6]
+    res = t.twoSum(nums, 9)
+    print(res)
